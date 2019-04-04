ec1c94ceb9bcfc4572de560bcb7bfa1efd32bf85
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2019-04-04 13:09:41 -0400

    change

--- Student ADT/src/StudentADTbyVV.java
+++ Student ADT/src/StudentADTbyVV.java
@@ -2,6 +2,8 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JSpinner;
@@ -10,6 +12,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.DefaultComboBoxModel;
 
 public class StudentADTbyVV {
 
@@ -82,6 +85,7 @@ public class StudentADTbyVV {
 		frmStudent.getContentPane().add(tctcMiddle);
 		
 		JComboBox comboBoxGrade = new JComboBox();
+		comboBoxGrade.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
 		comboBoxGrade.setMaximumRowCount(12);
 		comboBoxGrade.setBounds(134, 159, 85, 25);
 		frmStudent.getContentPane().add(comboBoxGrade);
@@ -101,11 +105,64 @@ public class StudentADTbyVV {
 		frmStudent.getContentPane().add(checkBox);
 		
 		JButton btnEnter = new JButton("ENTER");
+		
+		//when enter button is pressed get the information that was entered
 		btnEnter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
+				//define an object that will represent the member class
+				Member studInfo = new Member();
+			   
+				//get the last name and assign it to studInfo.lastName
+				studInfo.lastName = txtLast.getText();
+				
+				//get the first name and assign it to studInfo.firstName
+				studInfo.firstName = txtFirst.getText();
+				
+				//get the middle liter and assign it to studInfo.middleName
+				try
+				{
+					studInfo.middleName = (tctcMiddle.getText()).charAt(0);
+				}
+				catch(Exception e1) {
+					  //  Block if its not a character
+					JOptionPane.showMessageDialog(null, "Please enter only the initial");	
+				}
+				
+				//get the grade and assign it to studInfo.grade
+				studInfo.grade = comboBoxGrade.getSelectedIndex()+1;
+				
+				//check if they identified or not
+				
+				if (checkBox.isSelected())
+				{
+					studInfo.identify = "identified";
+				}
+				else
+				{
+					studInfo.identify = "not identified";
+				}
+				
+				//call the display message box function
+				studInfo.DisplayInfo();
 			}
 		});
 		btnEnter.setBounds(10, 258, 89, 23);
 		frmStudent.getContentPane().add(btnEnter);
 	}
+	
+	//create a class and populate it with variables
+	class Member
+	{
+		public String lastName;
+		public String firstName;
+		public char middleName;
+		public int grade;
+		public String identify;
+		
+		public void DisplayInfo()
+		{
+			JOptionPane.showMessageDialog(null, lastName + " "+ middleName + " "+ firstName + " is in grade " + grade + " and is " + identify);	
+		}
+	};
 }
